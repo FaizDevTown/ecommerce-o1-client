3db6496e3a9bb6bd001ea61337c8cf49a0a2024d
faiz <faiz@hexabells.com>
2023-11-30 15:33:56 +0530

    first commit

---
+++ src/components/ProductPreview.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Badge, Card } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+
+function ProductPreview({ _id, category, name, pictures,photo }) {
+    return (
+        <LinkContainer to={`/product/${_id}`} style={{ cursor: "pointer", width: "13rem", margin: "10px" }}>
+            <Card style={{ width: "20rem", margin: "10px" }}>
+                <Card.Img variant="top" className="product-preview-img" src={photo} style={{ height: "150px", objectFit: "cover" }} />
+                <Card.Body>
+                    <Card.Title>{name}</Card.Title>
+                    <Badge bg="warning" text="dark">
+                        {category}
+                    </Badge>
+                </Card.Body>
+            </Card>
+        </LinkContainer>
+    );
+}
+
+export default ProductPreview;
